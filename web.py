2fc9ff53bfdfba839230d57490bc84773604f5d5
Trevor <tdm67@cornell.edu>
2016-07-12 17:29:54 -0500

    adds prep_for_lda function, sites.txt cannot have blank lines

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 This Python script will take in a website at the command line
 , fetch the page, extract the paragraphs, and join them into
@@ -19,22 +20,61 @@ from gensim import corpora, models
 import gensim
 
 
-with open("sites.txt") as f:
-	#content is list of sites to extract text from
-	content = [line.strip('\n') for line in f]
-
-
-r = requests.get(url)
-
-data = r.text
-
-soup = BeautifulSoup(data, "html.parser")
-
-par_list=[]
-
-for paragraph in soup.find_all('p'):
-	par_list.append(paragraph.get_text())
+class App(object):
 	
-doc = " ".join(par_list)
+	def __init__(self):
+		self.urls = []
+		self.texts = []
+		self.prepped_texts = []
+		self.tokenizer = RegexpTokenizer(r'\w+')
+		self.stops = get_stop_words('en')
+		self.stemmer = PorterStemmer()
+		
 
+	def get_urls(self, fname):
+		with open(fname) as f:
+			#content is list of sites to extract text from
+			content = [line.strip('\n') for line in f]
+		self.urls.extend(content)
 
+	def parse(self, url):
+		r = requests.get(url)
+
+		data = r.text
+
+		soup = BeautifulSoup(data, "html.parser")
+
+		pars = ""
+
+		for paragraph in soup.find_all('p'):
+			pars+=paragraph.get_text()
+	
+		return pars
+		
+		
+	def get_texts(self):
+		
+		for u in self.urls:
+			self.texts.append(self.parse(u))
+			
+	def prep_for_lda(self):
+		#tokenize, stop, stem
+		for t in self.texts:
+			t = t.lower()
+			tokens= self.tokenizer.tokenize(t)
+			stopped_tokens = [i for i in tokens if not i in self.stops]
+			stems = [self.stemmer.stem(i) for i in stopped_tokens]
+			self.prepped_texts.append(stems)
+		
+	
+	
+
+if __name__ == '__main__':
+	print("weeee")
+	app = App()
+	app.get_urls("sites.txt")
+	app.get_texts()
+	app.prep_for_lda()
+	
+	
+	
